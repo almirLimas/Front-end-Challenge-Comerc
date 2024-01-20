2978ae9f45859e9309e406ab4103cdd00c3516e5
almirLimas <104025405+almirLimas@users.noreply.github.com>
2024-01-20 17:52:06 -0300

    add components cards

---
+++ challenge-locadora/src/components/Card.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-white rounded-lg p-8 m-3 hover:bg-zinc-100 cursor-pointer">
+    <div class="grid lg:grid-cols-12 gap-2">
+      <div class="col-span-12 flex justify-center items-center">
+        <img
+          src="../assets/Icons/user.svg"
+          alt="ìmagem de um usuário"
+          class="w-8"
+        />
+      </div>
+      <div class="col-span-12 flex justify-center">
+        <p>Usuário</p>
+      </div>
+    </div>
+  </div>
+</template>
--- challenge-locadora/src/views/Home.vue
+++ challenge-locadora/src/views/Home.vue
@@ -1,17 +1,26 @@
 <template>
-  <Banner />
-  <h1>kkkk</h1>
-  <FormComponent />
+  <div class="flex justify-center items-center h-[85vh]">
+    <div class="container bg-black m-8 h-5/6 shadow-xl rounded-lg">
+      <div class="grid lg:grid-cols-4 md:grid-cols-1 gap-4 shadow-xl">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import Banner from "./Banner.vue";
 import FormComponent from "../components/FormComponent.vue";
+import Card from "@/components/Card.vue";
 
 export default {
   components: {
     Banner,
     FormComponent,
+    Card,
   },
 };
 </script>
